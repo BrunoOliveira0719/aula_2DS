1e659ff288e653d3336e4d3d6251896431ec5251
BRUNO HENRIQUE RIBEIRO DE OLIVEIRA <00001083962863sp@aluno.educacao.sp.gov.br>
2024-08-27 17:31:42 -0300

    sum_array.py

---
+++ sum_array.py
@@ -0,0 +1,10 @@
+joao = [300, 500, 200, 800]
+
+pedro = [200, 400, 500, 700]
+
+if sum(joao) < sum(pedro):
+    print('Pedro gastou mais! ')
+elif sum(joao) > sum(pedro):
+    print('João gastou mais! ')
+else:
+    print('Eles gastaram a mesma coisa! ')
